e3ffade21d88836bd9a94c3fd982f3c3cc19d66b
Eugene Smolanka <smolanka.zhacka@gmail.com>
2011-12-13 21:01:58 +0200

    Added a simple example
    Ignore-this: a291e64933ee72bf635b4207ed9c141f
    
    darcs-hash:20111213190158-722dc-0cf9e689a20216a138ae2d140fec8e7c4e8bd37f

---
+++ examples/ex1.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import System
+import System.Environment
+import System.Console.YAOP
+
+import Data.List
+import Data.Maybe
+
+-- | Options that are not mapped to data
+withoutData = [ dummy =: option [] ["help"] (NoA) "Help msg" (\_ _ -> print "help msg") ]
+
+-- | Options data structure. Should use record syntax, may have more than one constructor
+data Options = Options { optFileName :: FilePath
+                       , optCount :: Int
+                       , optStuff :: [Either Int String]
+                       } deriving (Show)
+
+-- | Default options
+defOptions = Options {optFileName = "default.txt", optCount = 0, optStuff = []}
+
+-- | This triggers YAOP's accessors generator, e.g.
+-- @modM_optFileName :: Monad m => (FilePath -> m FilePath) -> Options -> m Options@
+$(deriveModM ''Options)
+
+-- | Here we define a list of options that are mapped to Options
+optDesc = [
+  modM_optFileName =: option ['f'] ["filename"] (ReqA "FN") "Set some filename" (\arg x -> print arg >> return (fromMaybe "" arg)),
+  modM_optCount    =: option ['c'] ["count"] (OptA "N") "Set some count" (\arg x -> return $ fromMaybe 100 (read `fmap` arg)),
+  modM_optStuff    =: option ['s'] ["stuff"] (NoA) "Push \"foo\" to a list" (\arg x -> return $ (Right "foo" : x))
+  ]
+
+bothDesc = withoutData ++ optDesc
+
+main = do
+  (opts,args) <- parseOptions bothDesc defOptions "USAGE: ./ex1 [OPTIONS]" =<< getArgs
+  print opts
+  print args
